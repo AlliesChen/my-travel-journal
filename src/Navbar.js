4bd87bb1e62870b0440c60813ae835cff24a04ff
YPChen <cyphuaenng@pm.me>
2022-03-13 01:21:34 +0800

    Initial commit

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="nav-container">
+      <div>
+        <img src="./image/icon-earth.svg" alt="earth icon" />
+        <a href="">my travel journal</a>
+      </div>
+    </nav>
+  );
+}
